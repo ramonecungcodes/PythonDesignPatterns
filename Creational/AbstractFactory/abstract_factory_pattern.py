0985729dfad446c85b762cd40ce19fa5bee3f36f
Ramon Ecung <success@RamonEcung.com>
2024-09-05 21:07:51 -0400

    initial code for abstract factory

---
+++ Creational/AbstractFactory/abstract_factory_pattern.py
@@ -0,0 +1,212 @@
+from abc import ABC, abstractmethod
+from enum import Enum
+
+
+class ComputerEquipmentInterface(ABC):
+    @property
+    @abstractmethod
+    def name() -> str:
+        pass
+
+    @abstractmethod
+    def turn_on(self):
+        pass
+
+    @abstractmethod
+    def turn_off(self):
+        pass
+
+
+class MonitorEquipmentInterface(ABC):
+    @property
+    @abstractmethod
+    def name() -> str:
+        pass
+
+    @abstractmethod
+    def turn_on(self):
+        pass
+
+    @abstractmethod
+    def turn_off(self):
+        pass
+
+
+class WebcamEquipmentInterface(ABC):
+    @property
+    @abstractmethod
+    def name() -> str:
+        pass
+
+    @abstractmethod
+    def turn_on(self):
+        pass
+
+    @abstractmethod
+    def turn_off(self):
+        pass
+
+
+class MacMini(ComputerEquipmentInterface):
+    name = "Mac Mini"
+
+    def turn_on(self):
+        print(f"{self.name} has turned on!")
+
+    def turn_off(self):
+        print(f"{self.name} has turned off!")
+
+
+class MacbookAir(ComputerEquipmentInterface):
+    name = "Macbook Air"
+
+    def turn_on(self):
+        print(f"{self.name} has turned on!")
+
+    def turn_off(self):
+        print(f"{self.name} has turned off!")
+
+
+class MacbookPro(ComputerEquipmentInterface):
+    name = "Macbook Pro"
+
+    def turn_on(self):
+        print(f"{self.name} has turned on!")
+
+    def turn_off(self):
+        print(f"{self.name} has turned off!")
+
+
+class Monitor4k(MonitorEquipmentInterface):
+    name = "4k Monitor"
+
+    def turn_on(self):
+        print(f"{self.name} has turned on!")
+
+    def turn_off(self):
+        print(f"{self.name} has turned off!")
+
+
+class DualMonitors(MonitorEquipmentInterface):
+    name = "Dual 4k Monitors"
+
+    def turn_on(self):
+        print(f"{self.name} has turned on!")
+
+    def turn_off(self):
+        print(f"{self.name} has turned off!")
+
+
+class Webcam1080p(WebcamEquipmentInterface):
+    name = "1080p Webcam"
+
+    def turn_on(self):
+        print(f"{self.name} has turned on!")
+
+    def turn_off(self):
+        print(f"{self.name} has turned off!")
+
+
+class Webcam4k(WebcamEquipmentInterface):
+    name = "4k Webcam"
+
+    def turn_on(self):
+        print(f"{self.name} has turned on!")
+
+    def turn_off(self):
+        print(f"{self.name} has turned off!")
+
+
+class EmployeeEquipmentType(Enum):
+    REMOTE = 1
+    ONSITE = 2
+    EXECUTIVE = 3
+    CONTRACTOR = 4
+
+
+class EquipmentFactoryInterface(ABC):
+    """ Abstract factory """
+    @property
+    @abstractmethod
+    def employee_equipment_type():
+        pass
+
+    @abstractmethod
+    def define_monitor(self) -> MonitorEquipmentInterface:
+        pass
+
+    @abstractmethod
+    def define_webcam(self) -> WebcamEquipmentInterface:
+        pass
+
+    @abstractmethod
+    def define_computer(self) -> ComputerEquipmentInterface:
+        pass
+
+
+class RemoteEquipmentFactory(EquipmentFactoryInterface):
+    """ Factory """
+    employee_equipment_type = EmployeeEquipmentType.REMOTE
+
+    def define_monitor(self):
+        return Monitor4k()
+    
+    def define_computer(self):
+        return MacbookAir()
+    
+    def define_webcam(self):
+        return Webcam1080p()
+
+
+class OnsiteEquipmentFactory(EquipmentFactoryInterface):
+    employee_equipment_type = EmployeeEquipmentType.ONSITE
+
+    def define_monitor(self):
+        return DualMonitors()
+    
+    def define_computer(self):
+        return MacMini()
+    
+    def define_webcam(self):
+        return Webcam1080p()
+
+
+class ExecutiveEquipmentFactory(EquipmentFactoryInterface):
+    employee_equipment_type = EmployeeEquipmentType.EXECUTIVE
+
+    def define_monitor(self):
+        return DualMonitors()
+    
+    def define_computer(self):
+        return MacMini()
+    
+    def define_webcam(self):
+        return Webcam1080p()
+
+
+class ContractorEquipmentFactory(EquipmentFactoryInterface):
+    employee_equipment_type = EmployeeEquipmentType.CONTRACTOR
+
+    def define_monitor(self):
+        return Monitor4k()
+    
+    def define_computer(self):
+        return MacMini()
+    
+    def define_webcam(self):
+        return Webcam1080p()
+
+
+def demo_abstract_factory():
+    new_employee_equipment = ExecutiveEquipmentFactory()
+
+    computer = new_employee_equipment.define_computer()
+    monitor = new_employee_equipment.define_monitor()
+    webcam = new_employee_equipment.define_webcam()
+
+    computer.turn_on()
+    monitor.turn_on()
+    webcam.turn_on()
+
+if __name__ == "__main__":
+    demo_abstract_factory()
